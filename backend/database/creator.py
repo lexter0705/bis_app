dbf597eddaa8df707d7465c234894a1ddfc860fc
C418418 <friday888777@gmail.com>
2024-12-26 14:15:50 +0300

    .

--- backend/database/creator.py
+++ backend/database/creator.py
@@ -6,13 +6,6 @@ from sqlalchemy.testing.schema import mapped_column
 class Base(DeclarativeBase):
     pass
 
-
-class MessageTypes(Base):
-    __tablename__ = 'message_types'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
-
-
 class UserTable(Base):
     __tablename__ = 'user'
     user_id: Mapped[str] = mapped_column(primary_key=True)
@@ -37,46 +30,6 @@ class MessageTable(Base):
     time: Mapped[int]
     user: Mapped[UserTable] = relationship(UserTable, backref=backref("children", cascade="all,delete"))
     chat: Mapped[ChatTable] = relationship(ChatTable, backref=backref("children", cascade="all,delete"))
-
-
-class Calls(Base):
-    __tablename__ = "calls"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    time: Mapped[int]
-    chat_id: Mapped[int] = mapped_column(ForeignKey('chat.id'))
-    chat: Mapped[ChatTable] = relationship(ChatTable, backref=backref("children", cascade="all,delete"))
-
-
-class StickerPacks(Base):
-    __tablename__ = 'sticker_packs'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    owner_address: Mapped[str] = mapped_column(ForeignKey('user.user_id'))
-    user: Mapped[UserTable] = relationship(UserTable, backref=backref("children", cascade="all,delete"))
-
-
-class Stickers(Base):
-    __tablename__ = 'stickers'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    pack_id: Mapped[str] = mapped_column(ForeignKey('pack.id'))
-    image_path: Mapped[str]
-    pack: Mapped[StickerPacks] = relationship(StickerPacks, backref=backref("children", cascade="all,delete"))
-
-
-class ReactionTypes(Base):
-    __tablename__ = "reaction_types"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    emoji: Mapped[str]
-
-
-class Reactions:
-    __tablename__ = "reactions"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    reaction_type: Mapped[int] = mapped_column(ForeignKey('message.id'))
-    from_user_id: Mapped[int] = mapped_column(ForeignKey('message.id'))
-    message_id: Mapped[str] = mapped_column(ForeignKey('message.id'))
-    message: Mapped[MessageTable] = relationship(MessageTable, backref=backref("children", cascade="all,delete"))
-    user: Mapped[UserTable] = relationship(UserTable, backref=backref("children", cascade="all,delete"))
-    reaction: Mapped[ReactionTypes] = relationship(ReactionTypes, backref=backref("children", cascade="all,delete"))
 
 
 def create_database(path: str):
